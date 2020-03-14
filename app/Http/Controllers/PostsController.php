bd04ecd478e3cbd16851ac6ce8e03bd0872a3a84
Shushanna <shushanna.karapetyan.97@gmail.com>
2020-03-14 23:59:12 +0400

    setup a db connection

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,18 +6,14 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post){
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
+    public function show($slug){
 
-        if(!array_key_exists($post,$posts)){
-            abort(404,"Sorry, that post was not found.");
-        }
+        $post = \DB::table('posts')->where('slug',$slug )->first();
+
+        //dd($post);
 
         return view('post', [
-            'post' => $posts[$post]  ?? "Nothing here yet."    //url is /posts/my-first-post ,  $posts['my-first-post']
+            'post'=>$post
         ]);
     }
 }
