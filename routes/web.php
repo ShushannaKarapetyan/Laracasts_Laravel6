f17c3a2ba62f344627110a47f181bde4a98632b1
Shushanna <shushanna.karapetyan.97@gmail.com>
2020-03-14 22:13:45 +0400

    route wildcards

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,22 @@ Route::get('/', function (Request $request) {
 
     //url   /?name=<script>alert("hi");</script>
 });
+
+// {post} is any variable which coming from url, f.e id of post
+// url /posts/1
+Route::get('posts/{post}', function ($post){
+    //return $post;
+
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+
+    if(!array_key_exists($post,$posts)){
+        abort(404,"Sorry, that post was not found.");
+    }
+
+    return view('post', [
+        'post' => $posts[$post]  ?? "Nothing here yet."    //url is /posts/my-first-post ,  $posts['my-first-post']
+    ]);
+});
