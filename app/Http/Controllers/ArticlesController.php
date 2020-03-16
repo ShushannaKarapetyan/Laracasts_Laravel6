7c424e3569fabf0af7bf961b36d11a2a92336c23
Shushanna <shushanna.karapetyan.97@gmail.com>
2020-03-16 12:49:59 +0400

    The Seven Restful Controller Actions

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,14 +8,41 @@ use Illuminate\Http\Request;
 class ArticlesController extends Controller
 {
 
+    //Render a list of a resource
     public function index(){
         $articles = Article::all();
         return view('articles.index',['articles'=>$articles]);
     }
 
+    //Show a single resource
     public function show($id){
         $article = Article::find($id);
         return view('articles.show_article',['article'=>$article]);
+    }
+
+    //Shows a view to create a new resource
+    public function create(){
+
+    }
+
+    //Persist the new resource
+    public function store(){
+
+    }
+
+    //Shows a view to edit an existing resource
+    public function edit(){
+
+    }
+
+    //Persist the edited resource
+    public function update(){
+
+    }
+
+    //Delete the resource
+    public function destroy(){
+
     }
 
 
