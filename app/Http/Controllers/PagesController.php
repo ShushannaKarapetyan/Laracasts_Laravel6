8a94f973ba71cb809c6c5f5e35c7d4d983bb26f1
Shushanna <shushanna.karapetyan.97@gmail.com>
2020-03-18 15:21:05 +0400

    Laravel Facades Demystified

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Example;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
 
 class PagesController extends Controller
 {
-    public function home(/*Example $example*/){
+    public function home(/*Example $example*/ Filesystem $file){
         /*ddd($example);*/
 
         //if we use singleton() instead of bind() in AppServiceProvider->register() , we get same objects
-        ddd(resolve('App\Example'),resolve('App\Example'),resolve('App\Example'));
+/*        ddd(resolve('App\Example'),resolve('App\Example'),resolve('App\Example'));*/
+
+
+
+
+        /*return File::get(public_path('index.php'));*/
+
+        #it's the same
+
+        return $file->get(public_path('index.php'));
+
+        /*Cache::remember('foo', 60, fn() => 'Something' );
+        return Cache::get('foo');*/
+
     }
 }
